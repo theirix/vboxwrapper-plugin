d6585238e35b11b18a0d6e69187064253ba8992d
theirix <theirix@gmail.com>
2012-10-14 18:13:43 +0400

    Removed build steps, introduced global commands

--- src/main/java/org/jenkinsci/plugins/VBoxWrapper.java
+++ src/main/java/org/jenkinsci/plugins/VBoxWrapper.java
@@ -3,23 +3,26 @@ package org.jenkinsci.plugins;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.BuildListener;
-import hudson.model.Describable;
+import hudson.model.TaskListener;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.tasks.BuildStep;
+import hudson.model.Node;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
-import hudson.tasks.Builder;
+import hudson.tasks.Shell;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.List;
+
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
- * Sample {@link Builder}.
+ * BuildWrapper
  * 
  * <p>
  * When the user configures the project and enables this builder,
@@ -28,35 +31,34 @@ import org.kohsuke.stapler.StaplerRequest;
  * project configuration XML by using XStream, so this allows you to use
  * instance fields (like {@link #name}) to remember the configuration.
  * 
- * <p>
- * When a build is performed, the
- * {@link #perform(AbstractBuild, Launcher, BuildListener)} method will be
- * invoked.
- * 
- * @author Kohsuke Kawaguchi
+ * @author theirix
  */
 
 @SuppressWarnings("rawtypes")
 public class VBoxWrapper extends BuildWrapper {
 
-	private final ArrayList<BuildStep> vboxSetupSteps;
-	private final ArrayList<BuildStep> vboxTeardownSteps;
-
-	// Fields in config.jelly must match the parameter names in the
-	// "DataBoundConstructor"
+	private final List<String> virtualSlaves;
+	private final boolean useSetup;
+	private final boolean useTeardown;
+	
 	@DataBoundConstructor
-	public VBoxWrapper(ArrayList<BuildStep> vboxSetupSteps,
-			ArrayList<BuildStep> vboxTeardownSteps) {
-		this.vboxSetupSteps = vboxSetupSteps;
-		this.vboxTeardownSteps = vboxTeardownSteps;
+	public VBoxWrapper(List<String> virtualSlaves, boolean useSetup,
+			boolean useTeardown) {
+		this.virtualSlaves = virtualSlaves;
+		this.useSetup = useSetup;
+		this.useTeardown = useTeardown;
 	}
 
-	public ArrayList<BuildStep> getVboxSetupSteps() {
-		return vboxSetupSteps;
+	public List<String> getVirtualSlaves() {
+		return virtualSlaves;
 	}
 
-	public ArrayList<BuildStep> getVboxTeardownSteps() {
-		return vboxTeardownSteps;
+	public boolean isUseSetup() {
+		return useSetup;
+	}
+
+	public boolean isUseTeardown() {
+		return useTeardown;
 	}
 
 	/**
@@ -65,7 +67,7 @@ public class VBoxWrapper extends BuildWrapper {
 	class VBoxEnvironment extends Environment {
 
 		private final Launcher launcher;
-		
+
 		public VBoxEnvironment(Launcher launcher) {
 			this.launcher = launcher;
 		}
@@ -78,69 +80,144 @@ public class VBoxWrapper extends BuildWrapper {
 		@Override
 		public boolean tearDown(AbstractBuild build, BuildListener listener)
 				throws IOException, InterruptedException {
-			
-			invokeBuildSteps(build, launcher, vboxTeardownSteps, listener);
+
+			if (isUseTeardown())
+				invokeVBoxCommand(getDescriptor().getTeardownCommand(), build, launcher, listener);
 			return true;
 		}
 
 	}
-	
 
-	/*
-	 * Default impl
-	 * 
-	 * @see hudson.tasks.BuildWrapper#setUp(hudson.model.AbstractBuild,
-	 * hudson.Launcher, hudson.model.BuildListener)
-	 */
 	@Override
 	public Environment setUp(AbstractBuild build, Launcher launcher,
 			BuildListener listener) throws IOException, InterruptedException {
 
-		invokeBuildSteps(build, launcher, vboxSetupSteps, listener);
+		dumpSettings(listener);
+		
+		if (isUseSetup())
+			invokeVBoxCommand(getDescriptor().getSetupCommand(), build, launcher, listener);
+		
 		return new VBoxEnvironment(launcher);
 	}
+	
+	/*
+	 * Invoke shell command on master to setup/teardown selected virtual machines
+	 */
+	private void invokeVBoxCommand (String body, AbstractBuild build, Launcher launcher, BuildListener listener) throws InterruptedException {
+		if (body == null || body.equals(""))
+			return;
+		
+		StringBuilder sb = new StringBuilder(body);
+		for (String slave: getVirtualSlaves()) {
+			sb.append(" ");
+			sb.append(slave);
+		}
+		String commandLine = sb.toString();
+		listener.getLogger().println("Expect to launch command " + commandLine);
+		Shell shell = new Shell(commandLine);
+		if (!shell.perform(build, launcher, listener))
+			listener.error("VBox setup shell failed");		
+	}
 
-	private void invokeBuildSteps(AbstractBuild build, Launcher launcher, ArrayList<BuildStep> buildSteps, BuildListener listener) 
+	private void dumpSettings(BuildListener listener) {
+		listener.getLogger().format("useSetup %b\n", isUseSetup());
+		listener.getLogger().format("useTeardown %b\n", isUseTeardown());
+		listener.getLogger().format("setup command %s\n", getDescriptor().getSetupCommand());
+		listener.getLogger().format("teardown command %s\n", getDescriptor().getTeardownCommand());
+	}
+
+	/*private void invokeBuildSteps(AbstractBuild build, Launcher launcher,
+			ArrayList<BuildStep> buildSteps, BuildListener listener)
 			throws InterruptedException, IOException {
+
+		if (virtualSlaves != null) {
+			for (String slaveName : virtualSlaves) {
+				listener.getLogger().format("Using slave: %s\n", slaveName);
+			}
+		}
+
 		if (buildSteps == null) {
 			listener.getLogger().println("No build steps declared");
 			return;
 		}
-		
+
 		for (BuildStep bs : buildSteps) {
-			listener.getLogger().format("Invoking prebuild step '%s'\n", getBuildStepName(bs));
-            if (!bs.prebuild(build,listener)) {
-            	listener.getLogger().println(MessageFormat.format("{0} : {1} failed", build.toString(), getBuildStepName(bs)));
-                return;
-            }
+			listener.getLogger().format("Invoking prebuild step '%s'\n",
+					getBuildStepName(bs));
+			if (!bs.prebuild(build, listener)) {
+				listener.getLogger().println(
+						MessageFormat.format("{0} : {1} failed",
+								build.toString(), getBuildStepName(bs)));
+				return;
+			}
 		}
-		
+
 		for (BuildStep bs : buildSteps) {
-			listener.getLogger().format("Invoking build step '%s'\n", getBuildStepName(bs));
+			listener.getLogger().format("Invoking build step '%s'\n",
+					getBuildStepName(bs));
 			bs.perform(build, launcher, listener);
 		}
 	}
-	
+
 	private String getBuildStepName(BuildStep bs) {
 		if (bs instanceof Describable<?>) {
-             return ((Describable<?>) bs).getDescriptor().getDisplayName();
-         } else {
-             return bs.getClass().getSimpleName();
-         }
+			return ((Describable<?>) bs).getDescriptor().getDisplayName();
+		} else {
+			return bs.getClass().getSimpleName();
+		}
 
-     }
-    
+	}*/
+
+	public static List<String> getSlaveNames() {
+		final List<Node> nodes = Jenkins.getInstance().getNodes();
+		List<String> test = new ArrayList<String>();
+
+		for (Node node : nodes) {
+			if (!node.getNodeName().equals("")
+					&& !node.getNodeName().equals("master"))
+				test.add(node.getNodeName());
+		}
+		return test;
+	}
+
 	@Override
-    public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
-    }
-    
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
+
 	/**
 	 * Descriptor for {@link VBoxWrapper}. Used as a singleton. The class is
 	 * marked as public so that it can be accessed from views.
 	 */
 	@Extension
 	public static final class DescriptorImpl extends BuildWrapperDescriptor {
+
+		private String setupCommand;
+		private String teardownCommand;
+		
+
+		public DescriptorImpl() {
+			super();
+			load();
+		}
+
+		public String getSetupCommand() {
+			return setupCommand;
+		}
+
+		public String getTeardownCommand() {
+			return teardownCommand;
+		}
+
+		
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject json)
+				throws FormException {
+			setupCommand = json.getString("setupCommand");
+			teardownCommand = json.getString("teardownCommand");
+			save();
+			return super.configure(req, json);
+		}
 
 		/**
 		 * This human readable name is used in the configuration screen.
